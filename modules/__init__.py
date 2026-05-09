8a23f95d20139a4d20061ad7776a5c97f271ceee
YaminiBR144 <yaminibr4@gmail.com>
2026-05-09 11:49:51 +0530

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,6 @@
+# modules/__init__.py
+from .camera import camera_translate
+from .image import image_translate
+from .voice import voice_translate
+from .text import text_translate
+from .history import save_history, load_history
